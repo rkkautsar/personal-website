ec6509127c90da994c2e36396fd3935d2f242c4c
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2018-06-14 02:44:52 +0700

    fix build error regarding dom and prerendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './style';
-import WebFont from 'webfontloader';
 import App from './components/app';
 
-WebFont.load({
-  google: {
-    families: ['Lekton:400,700']
-  }
-});
+if (typeof window !== "undefined") {
+  const WebFont = require('webfontloader');
+  WebFont.load({
+    google: {
+      families: ['Lekton:400,700']
+    }
+  });
+}
 
 export default App;
