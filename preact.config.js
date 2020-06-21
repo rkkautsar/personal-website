65cbed0fa91bdb5a13c9a9dad6370e9de6642d9e
Rakha Kanz Kautsar <rakha.kautsar@shopee.com>
2020-06-21 15:49:09 +0800

    refactor!: start anew

--- preact.config.js
+++
@@ -1,7 +0,0 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin'
-import criticalCssPlugin from "preact-cli-plugin-critical-css";
-
-export default (config, env) => {
-    config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: '*' }]) );
-    criticalCssPlugin(config, env);
-}
--- src/utils.js
+++
@@ -1,1 +0,0 @@
-export const getSVGIconUrl = name => `https://unpkg.com/simple-icons@latest/icons/${name}.svg`;
