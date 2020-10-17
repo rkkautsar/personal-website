601e43531312568f8b45766e1758f9dc1a3349c3
Rakha Kanz Kautsar <rakha.kautsar@shopee.com>
2020-10-17 22:35:02 +0800

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import "./style.css";
 
+if (typeof window !== "undefined") {
+  (async () => {
+    const { default: ReactGA } = await import("react-ga");
+
+    ReactGA.initialize("UA-20001041-2", {
+      debug: process.env.NODE_ENV === "development",
+    });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  })();
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
