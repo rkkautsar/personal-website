9b7afeb38e2bfcca90ad21201bf6525bdd056f84
Rakha Kanz Kautsar <rakha.kautsar@shopee.com>
2020-10-17 16:45:28 +0800

    fix: tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    "./lib/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
