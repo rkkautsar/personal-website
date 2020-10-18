d1aa354f96a5581777a5947ed788fa6fd32bae95
Rakha Kanz Kautsar <rakha.kautsar@shopee.com>
2020-10-18 12:39:09 +0800

    refactor: use firebase directly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,36 @@
 import "./style.css";
+import * as React from "react";
+import { useRouter } from "next/router";
 
+let ga: firebase.analytics.Analytics | null = null;
 if (typeof window !== "undefined") {
   (async () => {
-    const { default: ReactGA } = await import("react-ga");
-
-    ReactGA.initialize("UA-20001041-2", {
-      debug: process.env.NODE_ENV === "development",
-    });
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    const firebase = await import("lib/services/firebase");
+    ga = firebase.analytics;
   })();
 }
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (ga == null) return;
+      ga.setCurrentScreen(document.title);
+      ga.logEvent("page_view", {
+        page_location: router.asPath,
+        page_path: router.pathname,
+        page_title: document.title,
+      });
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
