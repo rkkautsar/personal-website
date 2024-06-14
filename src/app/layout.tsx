b5010ac0a929716c955e4d8df128f1dda64541e3
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2024-06-14 22:44:04 +0800

    add: cf analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import {workSans} from "@/app/fonts";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import type { Metadata } from 'next';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { workSans } from '@/app/fonts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: "Rakha Kanz Kautsasr",
-  description: "Rakha Kanz Kautsar, front-end software engineer @ TikTok",
+  title: 'Rakha Kanz Kautsasr',
+  description: 'Rakha Kanz Kautsar, front-end software engineer @ TikTok',
 };
 
 export default function RootLayout({
@@ -22,7 +22,11 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
       </body>
-
+      <Script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "c562bd6846a947bf9731e595b478ad27"}'
+      />
     </html>
   );
 }
